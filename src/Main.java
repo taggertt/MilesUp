6f48715504f0a3b041636b863ef075edd6d942a4
taggertt <taggertt@mail.ru>
2022-07-07 21:31:10 +0500

    add description

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price);
+        int price = 10_000; // Цена на билеты
+        int miles = service.calculate(price); // Расчёт бонусных милей
         System.out.println(miles);
     }
 }
